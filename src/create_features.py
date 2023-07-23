7a3cfd16688befea1c6bbfb131595d640591d126
suikabar <mizuho.suikabar@gmail.com>
2023-07-23 08:13:39 +0000

    update features preprocessing

---
+++ src/create_features.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import polars as pl
+
+from preprocessing.features import FEATURES_DICT
+
+ROOT = Path(__file__).parents[1]
+TRAIN_PATH = ROOT / "data/raw_data/train_0-4.csv"
+FEATURES_STORE_DIR = ROOT / "data/features/"
+
+cols_basic = [
+    pl.col("page").cast(pl.Float32),
+    pl.col("fqid").fill_null("fqid_None"),
+    pl.col("text_fqid").fill_null("text_fqid_None"),
+    (
+        (pl.col("elapsed_time") - pl.col("elapsed_time").shift(1))
+        .fill_null(0)
+        .clip(0, 1e9)
+        .over(["session_id", "level"])
+        .alias("elapsed_time_diff")
+    ),
+    (
+        (pl.col("screen_coor_x") - pl.col("screen_coor_x").shift(1))
+        .abs()
+        .over(["session_id", "level"])
+        .alias("screen_coor_x_diff")
+    ),
+    (
+        (pl.col("screen_coor_y") - pl.col("screen_coor_y").shift(1))
+        .abs()
+        .over(["session_id", "level"])
+        .alias("screen_coor_y_diff")
+    ),
+]
+
+
+if __name__ == "__main__":
+    df = pl.read_csv(TRAIN_PATH)
+    df = df.with_columns(cols_basic)
+
+    # 特徴量ごとにparquet形式で特徴量ストアに保存
+    for label, features in FEATURES_DICT.items():
+        path = FEATURES_STORE_DIR / f"{label}.parquet"
+        if path.exists():
+            continue
+        else:
+            df_aggregated = df.groupby("session_id", maintain_order=True).agg(features)
+            df_aggregated.write_parquet(FEATURES_STORE_DIR / f"{label}.parquet")
---
+++ src/preprocessing/features.py
@@ -0,0 +1,86 @@
+from typing import Dict, List
+
+import polars as pl
+
+from .consts import dialogs
+
+# columns type
+cols_cat = ["event_name", "name", "fqid", "text", "room_fqid"]
+cols_num = [
+    "page",
+    "room_coor_x",
+    "room_coor_y",
+    "screen_coor_x",
+    "screen_coor_y",
+    "hover_duration",
+    "elapsed_time_diff",
+    "screen_coor_x_diff",
+    "screen_coor_y_diff",
+]
+
+""" Aggregated Features """
+FEATURES_DICT: Dict[str, List[pl.Expr]] = {}
+
+FEATURES_DICT["basic_aggs"] = [
+    pl.col("index").count().alias("session_count"),
+    (pl.col("elapsed_time").max() - pl.col("elapsed_time").min()).alias(
+        "total_time"
+    ),  # 経過時間
+    (
+        pl.col("index").count()
+        / (pl.col("elapsed_time").max() - pl.col("elapsed_time").min())
+    ).alias(
+        "click_freq"
+    ),  # クリックする頻度
+    *[pl.col(col).drop_nulls().n_unique().alias(f"{col}_nunique") for col in cols_cat],
+    *[pl.col(col).mean().alias(f"{col}_mean") for col in cols_num],
+    *[pl.col(col).sum().alias(f"{col}_sum") for col in cols_num],
+    *[pl.col(col).std().alias(f"{col}_std") for col in cols_num],
+    *[pl.col(col).min().alias(f"{col}_min") for col in cols_num],
+    *[pl.col(col).max().alias(f"{col}_max") for col in cols_num],
+]
+
+FEATURES_DICT["dialogs_aggs"] = [
+    *[
+        pl.col("index")
+        .filter(pl.col("text").str.contains(c))
+        .count()
+        .alias(f"word_{c}")
+        for c in dialogs
+    ],
+    *[
+        pl.col("elapsed_time_diff")
+        .filter((pl.col("text").str.contains(c)))
+        .mean()
+        .alias(f"word_mean_{c}")
+        for c in dialogs
+    ],
+    *[
+        pl.col("elapsed_time_diff")
+        .filter((pl.col("text").str.contains(c)))
+        .std()
+        .alias(f"word_std_{c}")
+        for c in dialogs
+    ],
+    *[
+        pl.col("elapsed_time_diff")
+        .filter((pl.col("text").str.contains(c)))
+        .max()
+        .alias(f"word_max_{c}")
+        for c in dialogs
+    ],
+    *[
+        pl.col("elapsed_time_diff")
+        .filter((pl.col("text").str.contains(c)))
+        .sum()
+        .alias(f"word_sum_{c}")
+        for c in dialogs
+    ],
+    *[
+        pl.col("elapsed_time_diff")
+        .filter((pl.col("text").str.contains(c)))
+        .median()
+        .alias(f"word_median_{c}")
+        for c in dialogs
+    ],
+]
